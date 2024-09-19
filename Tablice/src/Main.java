1d517eed390072bad76584cf777b86eb7eb2bb47
Sitorter <91287185+Sitorter@users.noreply.github.com>
2024-09-19 14:21:55 +0200

    Add files via upload

---
+++ Tablice/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[]flimy = new String[2];
+        flimy[0] = "1. pociąg";
+        flimy[1] = "2. żarówka";
+        System.out.println(flimy[0]);
+        System.out.println(flimy[1]);
+        }
+    }
